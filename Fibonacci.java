aa8ad175b10600af41aa85e847bb26f34e01b9c5
Sebastian Alejos <bastialejos.1997@gmail.com>
2020-02-13 12:48:39 -0600

    Se agrega función de preparación y se optimiza la funcion

--- Fibonacci.java
+++ Fibonacci.java
@@ -7,12 +7,28 @@ public class Fibonacci {
 			return fibonacci(n-1)+fibonacci(n-2);
 		}
 	}
+	
+	//Función de preparación
+	public static long fibonacciOpt(int n) {
+		long[]res=new long[n+1];
+		res[0]=res[1]=1;
+		return fibonacciOpt1(n,res);
+	}
+	
+	private static long fibonacciOpt1(int n, long[] res) {
+		if(res[n]!=0) {
+		}else {
+			res[n]=fibonacciOpt1(n-1,res)+fibonacciOpt1(n-2,res);
+		}
+		return res[n];
+	}
+	
 	public static void main(String[] args) {
-		long ini =System.nanoTime(),
-				fin;
-		System.out.println(Fibonacci.fibonacci(100));
-		fin=System.nanoTime();
-		System.out.println("Se tardó " + (fin-ini)/1000000000.0);
+		long ini1 =System.nanoTime(),
+				fin1;
+		System.out.println(Fibonacci.fibonacciOpt(2000));
+		fin1=System.nanoTime();
+		System.out.println("Se tardó " + (fin1-ini1)/1000000000.0);
 	}
 
 }
